beda984eed777e6824c7d5ae302e9b17239b113f
Christian <christiannmenezes@gmail.com>
2022-06-10 22:32:56 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso de PHP devclass</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
+
+<body>
+  <h2>Curso de PHP</h2>
+
+  <div class="container">
+    <?php require load(); ?>
+  </div>
+</body>
+
+</html>
